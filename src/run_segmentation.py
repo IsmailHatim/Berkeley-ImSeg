e62e56aff40da0118add9bdd59b4012ceebe385e
IsmailHatim <ismoil59600@gmail.com>
2025-03-05 16:04:29 +0100

    Added run pipeline

---
+++ src/run_segmentation.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+import sys
+import os
+import importlib
+from argparse import ArgumentParser
+from utils import load_image, load_gt, compute_jaccard_score, compute_boundary_recall
+
+sys.dont_write_bytecode = True
+
+def main(args):
+    ROOT = os.getcwd()
+    INDEX = args.index
+    image_path = f'{args.image_path}\\{INDEX}.jpg'
+    ground_path = f'{args.ground_path}\\{INDEX}.mat'
+    method = getattr(importlib.import_module(f'segmentation.{args.method}'), args.method)
+
+    original_image = load_image(image_path)
+    segmented_image, best_t = method(original_image)
+    gt_segmentation_bin = load_gt(ground_path)
+
+    jaccard_score = compute_jaccard_score(segmented_image, gt_segmentation_bin)
+    recall_score = compute_boundary_recall(segmented_image, gt_segmentation_bin)
+    print("+"+"-"*50+"+")
+    print(f"Jaccard Score : {jaccard_score}\nBoundary Recall Score : {recall_score}")
+    print("+"+"-"*50+"+")
+
+    plt.figure(figsize=(12, 6))
+    plt.subplot(1, 3, 1), plt.imshow(original_image, cmap='gray'), plt.title('Original Image')
+    plt.subplot(1, 3, 2), plt.imshow(segmented_image, cmap='gray'), plt.title('Segmented Image')
+    plt.subplot(1, 3, 3), plt.imshow(gt_segmentation_bin, cmap='gray'), plt.title('Ground Truth Segmentation')
+    plt.show()
+
+    
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description='Run selected segmentation method on Berkeley Dataset.')
+
+    parser.add_argument('--method', default='otsu', type=str, help='Segmentation method to use.')
+    parser.add_argument('--index', default='24063', type=int, help='Image index from Berkeley Dataset.')
+    parser.add_argument('--image_path', default='data\\images\\train', type=str, help='Images path.')
+    parser.add_argument('--ground_path', default='data\\ground_truth\\train', type=str, help='Ground truth path.')
+
+    args = parser.parse_args()
+    
+    main(args)
